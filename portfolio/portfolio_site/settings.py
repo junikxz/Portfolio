8f408fc2925834856ee531136ab79883cd1e11b1
junikxz <152830250+junikxz@users.noreply.github.com>
2025-08-30 17:31:32 +0700

    add vercel.json and update settings

--- portfolio/portfolio_site/settings.py
+++ portfolio/portfolio_site/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+
+ALLOWED_HOSTS = ["*"]  # atau bisa tambahin domain vercel nanti
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
